4e502e161aaa0993c3626c74c3d5c3c800cf35a2
Abhishek Kumar <AK_0385@analyttica.com>
2025-03-11 15:21:54 +0530

    fix:globel search functionalty

--- src/components/BookTile.jsx
+++ src/components/BookTile.jsx
@@ -3,15 +3,15 @@ import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import {useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 
 import Ribbon from "./Ribbon";
 import Options from "./Options";
 
 export default function MediaCard({ title, url, genre, id }) {
   const navigate = useNavigate();
-  const mode=useSelector((state)=>state.modeSlice.value)
-
+  const mode = useSelector((state) => state.modeSlice.value);
+  const searchValue=useSelector((state)=>state.searchInputValue.value)
   return (
     <Card
       onClick={() => {
@@ -21,7 +21,7 @@ export default function MediaCard({ title, url, genre, id }) {
         position: "relative",
         maxWidth: "13rem",
         transition: "transform 0.3s ease-in-out",
-        cursor:'pointer',
+        cursor: "pointer",
         "&:hover": {
           transform: "scale(1.05)",
         },
@@ -30,7 +30,6 @@ export default function MediaCard({ title, url, genre, id }) {
     >
       <Box>
         <Ribbon genre={genre} />
-        
       </Box>
 
       <Box
@@ -51,15 +50,13 @@ export default function MediaCard({ title, url, genre, id }) {
           }}
         />
       </Box>
-      <Box sx={{display:"flex",alignItems:'center',padding:"0.5rem"}}>
-      <Box sx={{width:'97%' }}>
-        <Typography variant="body1" align="center">
-          {title}
-        </Typography>
-      </Box>
-      {mode==="User"?
-      <Options title={title}/>:""
-    }
+      <Box sx={{ display: "flex", alignItems: "center", padding: "0.5rem" }}>
+        <Box sx={{ width: "97%" }}>
+          <Typography variant="body1" align="center">
+            {title}
+          </Typography>
+        </Box>
+        {mode === "User" && !searchValue  ? <Options title={title} /> : ""}
       </Box>
     </Card>
   );
